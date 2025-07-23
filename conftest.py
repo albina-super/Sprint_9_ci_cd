7e80ed1bcbf7ca834ebcdda2137e0685d95c6593
Toktieva Albina <albinawork777@gmail.com>
2025-07-23 18:01:50 +0500

    Добавила конфиги для ci

--- conftest.py
+++ conftest.py
@@ -3,11 +3,18 @@ from selenium import webdriver
 
 from data import BASE_URL, MY_USER_DATA
 from pages.authorization_page import AuthorizationPage
-
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.set_capability("browserName", "chrome")
+    driver = webdriver.Remote(
+        command_executor="http://selenoid:4444/wd/hub",
+        options=options
+    )
     driver.maximize_window()
     driver.get(BASE_URL)
 
